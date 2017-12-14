7c6f8774cc54eab64d0e3c7fbd8412b41a7acc05
nandawk <nandawawank@gmail.com>
2017-12-15 05:55:30 +0700

    Mobile Programming

---
+++ app/src/main/java/com/madwak/crduandroid/DataHelper.java
@@ -0,0 +1,33 @@
+package com.madwak.crduandroid;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by nandawk on 12/11/17.
+ */
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('1', 'Darsiwan', '1996-07-12', 'Laki-laki','Indramayu');";
+        db.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+    }
+}
